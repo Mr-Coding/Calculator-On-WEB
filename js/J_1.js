2d75681e4211fac08f0da6bb1f0d53ac16169793
ching <2900410568@qq.com>
2017-03-17 01:16:38 +0800

    6

---
+++ js/J_1.js
@@ -0,0 +1,48 @@
+//window.ontouchstart = function(e) { e.preventDefault(); }; 
+$(function(){
+	
+	var $expression  = $(".expression");
+	var $result      = $(".result");
+	$("#delete").click(function(){
+		
+	});
+	
+	var $one = $("#one").click(function(){
+		
+	});
+	var $two = $("#two").click(function(){
+		
+	});
+	var $three = $("#three").click(function(){
+		
+	});
+	var $four = $("#four").click(function(){
+		
+	});
+	var $five = $("#five").click(function(){
+		
+	});
+	var $six = $("#six").click(function(){
+		
+	});
+	var $seven = $("#seven").click(function(){
+		
+	});
+	var $eight = $("#eight").click(function(){
+		
+	});
+	var $nine = $("#nine").click(function(){
+		
+	});
+	var $zero = $("#zero").click(function(){
+		
+	});
+	
+})
+
+//判断能不能输入点号
+//判断第一个有没有数字以及判断最后一个是不是运算符
+//逐一删除的方法
+//List转为String
+//判断小数点之后是不是全是0，如是就包括小数点去掉
+//长按清除键全部删除
